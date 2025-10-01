fb3df4507b9fe8be0e564b44c3bf657cfb71aa1c
Akash Singh <laughing.buddha.lab@gmail.com>
2025-10-01 22:28:31 +0200

    add fonts

--- config/typography.js
+++ config/typography.js
@@ -19,47 +19,7 @@ const typographyConfig = {
       marginTop: 0.35, // Position from top
       textAlign: 'center'
     }
-  },
-  template2: {
-    font: {
-      family: 'Futura PT Light',
-      size: 38,
-      lineHeight: 1.5,
-      color: '#F5F2ED',
-      opacity: 0.85
-    },
-    attribution: {
-      family: 'Garamond',
-      style: 'italic',
-      size: 16,
-      color: '#8B7355'
-    },
-    layout: {
-      maxWidth: 0.75,
-      marginTop: 0.4,
-      textAlign: 'center'
-    }
-  },
-  template3: {
-    font: {
-      family: 'Futura PT Light',
-      size: 45,
-      lineHeight: 1.4,
-      color: '#F5F2ED',
-      opacity: 0.9
-    },
-    attribution: {
-      family: 'Garamond',
-      style: 'italic',
-      size: 20,
-      color: '#8B7355'
-    },
-    layout: {
-      maxWidth: 0.65,
-      marginTop: 0.33,
-      textAlign: 'center'
-    }
-  }
-}
+  }}
+
 
 module.exports = typographyConfig
