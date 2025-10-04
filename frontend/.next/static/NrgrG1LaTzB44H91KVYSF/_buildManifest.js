e627bcb92d997c000528f74b925f3b34da35e88b
Akash Singh <laughing.buddha.lab@gmail.com>
2025-10-04 18:07:53 +0200

    Remove Next.js cache files from version control

--- frontend/.next/static/NrgrG1LaTzB44H91KVYSF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-0cea1347c6582205.js"],"/_error":["static/chunks/pages/_error-d29bb199580a3119.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
